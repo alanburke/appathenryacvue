64c42f12a7221b74d35e2d3d9db68c8cf45ee320
Alan Burke <alan@cycletraffic.com>
2017-04-05 00:20:02 +0100

    Render images

--- src/main.js
+++ src/main.js
@@ -1,23 +1,48 @@
 import Vue from 'vue'
-import axios from 'axios';
+import axios from 'axios'
+import trimHTML from 'trim-html'
+import AsyncComputed from 'vue-async-computed'
+Vue.use(AsyncComputed)
 
 Vue.component('news-item', {
   props: ['item'],
-  template: '<li>{{ item.title }}</li>'
+  template: 
+    '<article>' +
+      '<h2>{{ item.title }}</h2>' +
+      '<img :src="image">' +
+      '<div v-html="teaser"></div>' +
+    '</article>',
+  computed: {
+    teaser: function () {
+      var teaser = trimHTML(this.item.body.value, {limit: 300, preserveTags: false})
+      return teaser.html
+    }
+  },
+  asyncComputed: {
+    image: function () {
+      if (this.item.field_news_image.length === undefined ) {
+        var uri = this.item.field_news_image.file.uri + '.json'
+        return axios.get(uri).then(response =>  response.data.url)
+      }
+      else {
+        return new Promise((resolve, reject) => {});
+      }
+    }
+  }
 })
 
 var app = new Vue({
   el: '#app',
-	data: {
-		news: [],
-	},
+  data: {
+    news: [],
+  },
   mounted () {
-    axios.get('https://wwwathenryaccom.docksal/node.json?sort=nid&direction=DESC&limit=10')
+    axios.get('https://wwwathenryaccom.docksal/node.json?sort=nid&direction=DESC&limit=10&promote=1')
     .then(response => {
       this.news = response.data.list
     })
     .catch(e => {
       this.errors.push(e)
     })
-	}
+  }
 })
